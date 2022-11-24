6666ca9aa27ff7bef54a12614d3d8f42021903fa
unknown <yronorio@gmail.com>
2022-11-24 10:53:57 -0300

    Deixando campos obrigatórios

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,3 +1,6 @@
+import { Usuario } from './../models/Usuario';
+import { comparaValidator } from './../validators/compara-validator';
+import { CpfValidator } from './../validators/cpf-validator';
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
@@ -9,21 +12,54 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class RegisterPage implements OnInit {
 
   formRegister: FormGroup;
+  usuario: Usuario = new Usuario();
+
+  mensagens = {
+    nome: [
+      { tipo: 'required', mensagem: 'O campo Nome é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
+    ],
+    cpf: [
+      { tipo: 'required', mensagem: 'O campo CPF é obrigatório.' },
+      { tipo: 'invalido', mensagem: 'CPF Inválido.' },
+    ],
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+      { tipo: 'email', mensagem: 'E-mail Inválido.' },
+    ],
+    password: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+    ],
+    confirmPassword: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+      { tipo: 'comparacao', mensagem: 'Deve ser igual a Senha anterior.' },
+    ],
+  };
 
   constructor(private formBuilder: FormBuilder) {
     this.formRegister = this.formBuilder.group({
       nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      cPF: ['', Validators.compose([Validators.required])],
+      cpf: ['', Validators.compose([Validators.required, CpfValidator.cpfValido])],
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
       confirmPassword: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+    }, {
+      validator: comparaValidator('password' , 'confirmPassword')
     });
     }
   ngOnInit() {
   }
 
   salvarRegister(){
-    console.log('Formulário: ',  this.formRegister.valid);
+    if(this.formRegister.valid){
+      this.usuario.nome = this.formRegister.value.nome;
+      this.usuario.cpf = this.formRegister.value.cpf;
+      this.usuario.nome = this.formRegister.value.nome;
+    }
   }
 
 }
