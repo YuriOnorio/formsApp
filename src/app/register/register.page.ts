3f06341bcc6dd6dbcdb6f867202819004f1ede17
Yuri Onório <101823180+YuriOnorio@users.noreply.github.com>
2022-11-24 08:56:02 -0300

    Deixando campos obrigatorios

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  formRegister: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.formRegister = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      cPF: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+      confirmPassword: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+    });
+    }
   ngOnInit() {
+  }
+
+  salvarRegister(){
+    console.log('Formulário: ',  this.formRegister.valid);
   }
 
 }
