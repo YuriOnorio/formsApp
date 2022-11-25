7dcdc2d771c2ed9a4887ea18a3f9b3ca9751c2c8
unknown <yronorio@gmail.com>
2022-11-25 08:35:10 -0300

    terminando o tab1

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,8 +1,11 @@
+import { StorageService } from './../services/storage.service';
+import { Storage } from '@ionic/storage-angular';
 import { Usuario } from './../models/Usuario';
 import { comparaValidator } from './../validators/compara-validator';
 import { CpfValidator } from './../validators/cpf-validator';
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -40,7 +43,7 @@ export class RegisterPage implements OnInit {
     ],
   };
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private storageService: StorageService, private route: Router) {
     this.formRegister = this.formBuilder.group({
       nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
       cpf: ['', Validators.compose([Validators.required, CpfValidator.cpfValido])],
@@ -54,11 +57,16 @@ export class RegisterPage implements OnInit {
   ngOnInit() {
   }
 
-  salvarRegister(){
+  async salvarRegister(){
     if(this.formRegister.valid){
       this.usuario.nome = this.formRegister.value.nome;
       this.usuario.cpf = this.formRegister.value.cpf;
-      this.usuario.nome = this.formRegister.value.nome;
+      this.usuario.email = this.formRegister.value.emial;
+      this.usuario.password = this.formRegister.value.password;
+      await this.storageService.set(this.usuario.email, this.usuario);
+      this.route.navigateByUrl('/tab1');
+    } else {
+      alert('Formulário inválido');
     }
   }
 
